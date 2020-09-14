0fb7c7fdff633771399ebe859c3009c412f79158
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-09-14 12:22:03 -0700

    test

---
+++ build/precache-manifest.21e074800d8f53bb6f954875f9a24b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33754ac9219e95be55913aa116d64a78",
+    "url": "/React-Currency-Converter/index.html"
+  },
+  {
+    "revision": "b92ded1b559f456cd018",
+    "url": "/React-Currency-Converter/static/css/main.44eaf1b3.chunk.css"
+  },
+  {
+    "revision": "66950ece9b1363e81df3",
+    "url": "/React-Currency-Converter/static/js/2.75623caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Currency-Converter/static/js/2.75623caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92ded1b559f456cd018",
+    "url": "/React-Currency-Converter/static/js/main.adfc9786.chunk.js"
+  },
+  {
+    "revision": "3f1829eeb418043a1d11",
+    "url": "/React-Currency-Converter/static/js/runtime-main.33d5ae83.js"
+  }
+]);
